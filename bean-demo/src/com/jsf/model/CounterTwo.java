cf34c8068238c7a48260229a2ee0b3c87ae4ec6b
Oscar Santamaria Venegas <osantamaria@snapfinance.com>
2019-01-06 14:06:43 -0600

    Calling methods on Managed Bean

---
+++ bean-demo/src/com/jsf/model/CounterTwo.java
@@ -0,0 +1,30 @@
+package com.jsf.model;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class CounterTwo {
+
+	private int value = 0;
+
+	public String increment() {
+		value++;
+		
+		return "student_two";
+	}
+
+	public CounterTwo() {
+		
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
---
+++ bean-demo/src/com/jsf/model/TourBean.java
@@ -0,0 +1,30 @@
+package com.jsf.model;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TourBean {
+
+	private String kindOfTour;
+	
+	public TourBean() {
+		
+	}
+		
+	public String getKindOfTour() {
+		return kindOfTour;
+	}
+
+	public void setKindOfTour(String kindOfTour) {
+		this.kindOfTour = kindOfTour;
+	}
+
+	public String startTheTour() {
+		if (kindOfTour != null && kindOfTour.equals("city")) {
+			return "city_tour";
+		}
+		else {
+			return "country_tour";
+		}
+	}
+}
