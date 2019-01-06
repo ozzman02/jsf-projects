ccae9a8c4a7a2396cb2d35ec3d64b8097efea84e
Oscar Santamaria Venegas <osantamaria@snapfinance.com>
2019-01-06 11:02:26 -0600

    Validate number range and length

---
+++ validate-demo/src/com/jsf/validate/StudentTwo.java
@@ -0,0 +1,57 @@
+package com.jsf.validate;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentTwo {
+
+	private String firstName;
+	private String lastName;
+	private String postalCode;
+	private int freePasses;
+	
+	// create no-arg constructor
+	public StudentTwo() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+}
+
+
+
+
+
+
+
